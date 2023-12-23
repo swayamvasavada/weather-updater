f737265feba156367b55ee656d6b6ac981106911
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2023-12-23 23:05:50 +0530

    Dependencies solved

--- util/weather.js
+++ util/weather.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 async function getWeather() {
     let weatherReport;
-    await axios.get('http://api.weatherapi.com/v1/current.json?key=9753e949d7ca4f08ae1162446232312&q=Delhi,India&aqi=no').then(weatherInfo => {
+    await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=Delhi,India&aqi=no`).then(weatherInfo => {
         const temp = 'Temperature of delhi is ' + weatherInfo.data.current.feelslike_c + ' C,\n';
         const wind = `Today's average wind speed is ${weatherInfo.data.current.wind_kph} towards ${weatherInfo.data.current.wind_dir},\n`;
         const precip = `Precepitation chances are ${weatherInfo.data.current.precip_in}.`
