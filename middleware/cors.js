5108d0e75d8c1259ce927a82f2f766d2173c481f
Swayam Vasavada <109085921+swayamvasavada@users.noreply.github.com>
2024-01-16 15:25:42 +0530

    Update cors.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,5 @@
 function enableCors(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
